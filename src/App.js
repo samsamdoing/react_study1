3de7d9ac90372dad753ad74e6c48dfa35cfae204
samsamdoing <psqkrtoa@gmail.com>
2022-07-04 23:52:10 +0900

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Link to="/">home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">user</Link>
+          
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about/" element={<About />} />
+          <Route path="users/*" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+
+function Home() {
+  const [menu, setMenu] = useState(1);
+
+
+  const menu1 = () => {
+    setMenu(1);
+
+  }
+
+  const menu2 = () => {
+    setMenu(2);
+  }
+  return (
+    <div>
+      <h2>Home</h2>
+
+      <button onClick={menu1}>메뉴1</button>
+      <button onClick={menu2}>메뉴2</button>
+
+      {
+        menu === 1 ?
+        <div>메뉴1번 페이지입니다</div>
+        :
+        <div>메뉴2번 페이지입니다</div>
+      }
+
+    </div>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
